733244525824a4e48bbdab981c7e25de684e5151
nofalbriansah <nofalbriansah@gmail.com>
2021-03-30 19:52:55 +0700

    ,

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String message = "ini Adalah Text";
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Anonymous Method"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(message),
+              RaisedButton(
+                  child: Text("Tekan Saya"),
+                  onPressed: () {
+                    setState(() {
+                      message = "Tombol Sudah di Tekan";
+                    });
+                  })
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
